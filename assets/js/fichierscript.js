66b5928abf29455030531a33cf680a71046a691b
remydegrave <bolieve.fr@gmail.com>
2020-11-12 14:30:15 +0100

    correction

--- assets/js/fichierscript.js
+++ assets/js/fichierscript.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-    $('#text').show();
-}
+$(function () { // équivaut à $(document).ready(function() {});
+$("#text").show(); // show fait apparaitre l'élément (fonction native)
+// $("#text").css("display", "block");// le changement de class css fait apparaitre l'élément
+// $("#text").removeAttr("hidden"); // suppression de l'attribut HTML hidden fait apparaitre l'élément (fonction native)
+// $("#text").toggle();//  permet d'afficher ou masquer l'élément (fonction native)
+});
